4be97b7eb4b04231f75df5ba0dca86a5cb11c11f
M. Zubair Kamran <engineerzubair20@gmail.com>
2025-10-07 15:41:07 +0500

    Data Types

---
+++ Day 2/3 questions.js
@@ -0,0 +1,21 @@
+// Q1 Predict the output
+console.log(null + 1);
+console.log("5" + 3);
+console.log("5" - 3);
+console.log(true + false); 
+
+// Q2 Check types:
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof 123n);
+
+//Q3 Truthy or Falsy?
+console.log(Boolean(0)); 
+console.log(Boolean("0")); 
+console.log(Boolean([])); 
+console.log(Boolean(undefined));
+
+// Q4 Write a function isEmpty(value) that checks if a given value is null , undefined , or ""
+//  Q5 Compare with loose vs strict?
+console.log(5 == "5"); // ?
+console.log(5 === "5"); // ?
